77e9ce30c67ab838202f2ffd1815adf86154cbc0
Ivan Egorov <www.Len244@gmail.com>
2021-05-09 14:41:33 +0300

    Added docs

--- db/db.php
+++ db/db.php
@@ -1,14 +1,28 @@
 <?php
+/**
+ * Connect to database and return connection
+ * 
+ * @throws Exception if there is an error durning connection
+ *
+ * @return mysql
+ */
 function initAndReturnDatabase(){
     $databaseConnection = new mysqli("localhost", "root", "", "denero_junior_test");
     if ($databaseConnection->connect_error){
-        die("Connection filed, error: " . $databaseConnection->connect_error);
+        throw new Exception("Connection filed, error: " . $databaseConnection->connect_error);
     }
     
     return $databaseConnection;
 }
 
-function joinTask($databaseConnection){
-    $databaseConnection->query("SELECT DISTINCT *  FROM users JOIN objects ON users.object_id = objects.id");
+/**
+ * Perform sql query from second task
+ *
+ * @param mysqli $databaseConnection
+ * @return mysqli_result
+ */
+function joinTask(mysqli $databaseConnection){
+    $result = $databaseConnection->query("SELECT DISTINCT *  FROM users JOIN objects ON users.object_id = objects.id");
+    return $result;
 }
 ?>
--- index.php
+++ index.php
@@ -1,13 +1,67 @@
 <?php
+
+/**
+ * Including file with database interaction
+ */
 require "./db/db.php";
+
+/**
+ * @package MainPackage
+ * @author Ivan Egorov <www.Len244@gmail.com>
+ * @version 1.0.0
+ */
+
+ /**
+ * Item class that represents one item from database
+ * @package MainPackage
+ * @author Ivan Egorov <www.Len244@gmail.com>
+ * @version 1.0.0
+ */
 final class Item {
 
+    /**
+     * Variable stores id of an Item
+     * @access private
+     * @var int
+     */
     private int $id;
+
+    /**
+     * Variable stores name of an Item
+     * @access private
+     * @var string
+     */
     private string $name;
+
+    /**
+     * Variable stores status of an Item
+     * @access private
+     * @var int
+     */
     private int $status;
+
+    /**
+     * Varibale represents if Item vas changed or not.
+     * @access private
+     * @var bool
+     */
     private bool $changed;
+
+    /**
+     * Stores database connection that we getting from
+     * @access private
+     * @var mysql
+     * @see ./db/db.php
+     */
     private $databaseConnection;
     
+    /**
+     * Item constructor
+     * It is saves id inside object, gets database connection
+     * And doing init.
+     *
+     * @param int $id ID of an item
+     */
     function __construct(int $id)
     {
         $this->id = $id;
@@ -15,20 +69,49 @@ final class Item {
         $this->init($id);
     }
 
+    /**
+     * Magic method that allow you to get properties of an item if them exists.
+     *
+     * @param string $name
+     * 
+     * @throws Exception if $name does not exists in Item.
+     * 
+     * @return int|string|bool|mysql
+     */
     function __get($name)
     {
+        if (property_exists($this, $name)){
         return $this->$name;
+        } else {
+            throw new Exception("Property $name does not exists");
+        }
     }
 
+    /**
+     * Magic method that allows you redefine parametrs of an Item.
+     * It will not redefine $id or $databaseConnection.
+     *
+     * @param string $name name of parametr
+     * @param int|string|bool $value depends on type of parametr
+     */
     function __set($name, $value)
     {
-        if (strcmp($name, "id") == 0){}
+        if (strcmp($name, "id") == 0 || strcmp($name, "databaseConnection") == 0){}
         else if (property_exists($this, $name) && gettype($this->name) == gettype($value)){
             $this->changed = true;
             $this->$name = $value;
         }
     }
     
+    /**
+     * Method init call only in constructor.
+     * You mustn't use it anywhere else.
+     * 
+     * Method fetch data from database and stores it in Item.
+     *
+     * @param integer $id
+     * @return void
+     */
     private function init(int $id){
 
         $object_query = $this->databaseConnection->query("SELECT name, status FROM objects WHERE id = $id");
@@ -51,6 +134,12 @@ final class Item {
 
     }
 
+    /**
+     * Method save saves changes into database.
+     * Before save it is check $changed variable.
+     *
+     * @return void
+     */
     public function save(){
         if($this->changed){
             $this->databaseConnection->query("UPDATE objects SET name = $this->name, status = $this->status WHERE id = $this->id");
